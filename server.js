7ac8c3056412c1e618b185da418a6d0b34950b83
luka00077 <abdalmlkto1@gmail.com>
2025-07-17 17:05:49 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+{
+  "name": "data-server",
+  "version": "1.0.0",
+  "description": "سيرفر بسيط يستقبل بيانات",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
